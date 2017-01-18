404d1696a6fb041a38dca071d7e6c6000e14240a
Jul <dubost.julien@orange.fr>
2017-01-18 12:19:17 +0100

    Bordereau Etat dans CommandeBean

--- src/main/java/fr/formation/gestioncolis/controller/BordereauController.java
+++ src/main/java/fr/formation/gestioncolis/controller/BordereauController.java
@@ -1,29 +1,17 @@
 package fr.formation.gestioncolis.controller;
 
-import java.io.Serializable;
-import java.util.List;
+import fr.formation.gestioncolis.bean.*;
+import fr.formation.gestioncolis.dao.*;
+import fr.formation.gestioncolis.entity.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import fr.formation.gestioncolis.bean.BordereauBean;
-import fr.formation.gestioncolis.bean.CommandeBean;
-import fr.formation.gestioncolis.bean.PaquetBean;
-import fr.formation.gestioncolis.bean.ProductBean;
-import fr.formation.gestioncolis.dao.BordereauDao;
-import fr.formation.gestioncolis.dao.EtatDao;
-import fr.formation.gestioncolis.dao.PaquetDao;
-import fr.formation.gestioncolis.dao.ProductDao;
-import fr.formation.gestioncolis.entity.Bordereau;
-import fr.formation.gestioncolis.entity.Commande;
-import fr.formation.gestioncolis.entity.Etat;
-import fr.formation.gestioncolis.entity.Paquet;
-import fr.formation.gestioncolis.entity.Product;
+import java.io.Serializable;
+import java.util.List;
 
 @ManagedBean
 @ViewScoped
@@ -40,66 +28,43 @@ public class BordereauController implements Serializable {
 	@ManagedProperty("#{bordereauDao}")
 	private BordereauDao bordereauDao;
 
-	private List<Bordereau> bordereaux;
+    @ManagedProperty("#{paquetDao}")
+    private PaquetDao paquetDao;
 
-	@ManagedProperty("#{commandeBean}")
-	private CommandeBean commandeBean;
+    @ManagedProperty("#{paquetBean}")
+    private PaquetBean paquetBean;
 
-	@ManagedProperty("#{etatDao}")
-	private EtatDao etatDao;
+    @ManagedProperty("#{productDao}")
+    private ProductDao productDao;
 
-	@ManagedProperty("#{paquetBean}")
-	private PaquetBean paquetBean;
+    @ManagedProperty("#{productBean}")
+    private ProductBean productBean;
 
-	/*
-	 * @ManagedProperty("#{etatBean}") private EtatBean etatBean;
-	 */
-	@ManagedProperty("#{paquetDao}")
-	private PaquetDao paquetDao;
+    private List<Bordereau> bordereaux;
 
-	@ManagedProperty("#{productBean}")
-	private ProductBean productBean;
+    @PostConstruct
+    public void _init(){
+        BordereauController.LOGGER.debug("Chargement de la liste des bordereaux.");
+        this.bordereaux = bordereauDao.readAll();
+    }
 
-	@ManagedProperty("#{productDao}")
-	private ProductDao productDao;
+    public void detail(final Integer id){
+        Bordereau bordereau = bordereauDao.read(id);
+        this.bordereauBean.setId(bordereau.getId());
+        this.bordereauBean.setCommande(bordereau.getCommandeBean());
+        this.bordereauBean.setDetail(bordereau.getDetail());
+        this.bordereauBean.setDateSignature(bordereau.getDateSignature());
 
-	@PostConstruct
-	public void _init() {
-		BordereauController.LOGGER
-				.debug("Chargement de la liste des bordereaux.");
-		this.bordereaux = this.bordereauDao.readAll();
-	}
+        final Paquet paquet = new Paquet();
+        paquet.setId(bordereauBean.getCommande().getPaquetBean().getId());
+        paquet.setColi(this.paquetDao.read(paquet.getId()).getColi());
+        this.paquetBean.setProduit(paquet.getColi());
 
-	public void detail(final Integer id) {
-		final Bordereau bordereau = this.bordereauDao.read(id);
-		this.bordereauBean.setId(bordereau.getId());
-		this.bordereauBean.setCommande(bordereau.getCommandeBean().getId());
-		this.bordereauBean.setDetail(bordereau.getDetail());
-		this.bordereauBean.setDateSignature(bordereau.getDateSignature());
-
-		final Commande commande = bordereau.getCommandeBean();
-		this.commandeBean.setId(commande.getId());
-		this.commandeBean.setDateCommande(commande.getDateCommande());
-		this.commandeBean.setDateEnvoi(commande.getDateEnvoi());
-		this.commandeBean.setAckSent(commande.getAckSent());
-		this.commandeBean.setAckReceived(commande.getAckReceived());
-
-		final Etat etat = commande.getEtatBean();
-		// etat.setId(commande.getEtatBean().getId());
-		// etat.setNom(this.etatDao.read(etat.getId()).getNom());
-		this.commandeBean.setEtat(etat);
-
-		final Paquet paquet = new Paquet();
-		paquet.setId(commande.getPaquetBean().getId());
-		paquet.setColi(this.paquetDao.read(paquet.getId()).getColi());
-		this.paquetBean.setProduit(paquet.getColi());
-
-		final Product product = new Product();
-		product.setId(paquet.getColi().getId());
-		product.setReference(
-				this.productDao.read(product.getId()).getReference());
-		this.productBean.setReference(product.getReference());
-	}
+        final Product product = new Product();
+        product.setId(paquet.getColi().getId());
+        product.setReference(this.productDao.read(product.getId()).getReference());
+        this.productBean.setReference(product.getReference());
+    }
 
 	public List<Bordereau> getBordereaux() {
 		return this.bordereaux;
@@ -117,17 +82,9 @@ public class BordereauController implements Serializable {
 		this.bordereaux = bordereaux;
 	}
 
-	public void setCommandeBean(final CommandeBean commandeBean) {
-		this.commandeBean = commandeBean;
-	}
-
-	public void setEtatDao(final EtatDao etatDao) {
-		this.etatDao = etatDao;
-	}
-
-	public void setPaquetBean(final PaquetBean paquetBean) {
-		this.paquetBean = paquetBean;
-	}
+    public void setPaquetBean(PaquetBean paquetBean) {
+        this.paquetBean = paquetBean;
+    }
 
 	public void setPaquetDao(final PaquetDao paquetDao) {
 		this.paquetDao = paquetDao;
@@ -137,8 +94,7 @@ public class BordereauController implements Serializable {
 		this.productBean = productBean;
 	}
 
-	public void setProductDao(final ProductDao productDao) {
-		this.productDao = productDao;
-	}
+
+
 
 }
