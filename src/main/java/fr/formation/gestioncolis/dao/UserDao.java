48773ca8093a1745d5236db8689eb9969b1e946e
Elvynia <elvynia@gmail.com>
2017-01-22 13:58:56 +0100

    Suppression d'une méthode inutile.

--- src/main/java/fr/formation/gestioncolis/dao/UserDao.java
+++ src/main/java/fr/formation/gestioncolis/dao/UserDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.persistence.TypedQuery;
 
 import fr.formation.gestioncolis.entity.User;
 
@@ -20,13 +19,6 @@ public class UserDao extends AbstractDao<User> {
 	@Override
 	public List<User> readAll() {
 		return this.readAll(User.class);
-	}
-
-	public User readByUsername(final String username) {
-		final TypedQuery<User> query = this.em
-				.createNamedQuery("User.findByUsername", User.class);
-		query.setParameter("username", username);
-		return query.getSingleResult();
 	}
 
 }
